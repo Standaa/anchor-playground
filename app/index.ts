a28b512c4be0aeb03707c208fe1e47fb64a99599
Standaa <stanislasdrg@gmail.com>
2021-04-10 14:55:01 +0100

    Initialize Anchor Playground

---
+++ app/index.ts
@@ -0,0 +1,98 @@
+import * as anchor from "@project-serum/anchor";
+
+import idl from "./idl";
+
+document.getElementById("test").onclick = testConnection;
+document.getElementById("launch").onclick = launchInstructions;
+
+const NETWORK_URL_KEY = "http://localhost:8899";
+const PROGRAM_ID_KEY = "ChRhVvwJUAtktDxCH53AcvPNsySDxhLKmdJCuv8L6YW8";
+
+const connection = new anchor.web3.Connection(NETWORK_URL_KEY);
+const programId = new anchor.web3.PublicKey(PROGRAM_ID_KEY);
+
+const opts: anchor.web3.ConfirmOptions = {
+  preflightCommitment: "singleGossip",
+  commitment: "singleGossip",
+};
+
+const seed = "testSeed";
+let userAccount: anchor.web3.Account;
+let userWallet: anchor.Wallet;
+let counter: anchor.web3.PublicKey;
+let provider: anchor.Provider;
+let program: anchor.Program;
+
+async function testConnection() {
+  let { blockhash } = await connection.getRecentBlockhash();
+  console.log(blockhash);
+
+  console.log("success");
+}
+
+async function launchInstructions() {
+  userAccount = await newAccountWithLamports(connection);
+  userWallet = new anchor.Wallet(userAccount);
+  provider = new anchor.Provider(connection, userWallet, opts);
+  program = new anchor.Program(idl, programId, provider);
+
+  counter = await anchor.web3.PublicKey.createWithSeed(
+    userWallet.publicKey,
+    seed,
+    program.programId
+  );
+
+  try {
+    await program.rpc.create(provider.wallet.publicKey, {
+      accounts: {
+        counter: counter,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [userAccount],
+      instructions: [
+        await anchor.web3.SystemProgram.createAccountWithSeed({
+          basePubkey: userWallet.publicKey,
+          fromPubkey: userWallet.publicKey,
+          lamports: 10e8,
+          newAccountPubkey: counter,
+          programId: program.programId,
+          seed: seed,
+          space: 8 + 24,
+        }),
+      ],
+    });
+
+    let counterAccount = await program.account.counter(counter);
+    console.log(counterAccount);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function newAccountWithLamports(
+  connection: anchor.web3.Connection,
+  lamports = 50e8
+): Promise<anchor.web3.Account> {
+  const account = new anchor.web3.Account();
+
+  let retries = 30;
+
+  console.log(`Request Airdrop...`);
+  await connection.requestAirdrop(account.publicKey, lamports);
+
+  while (true) {
+    await sleep(500);
+    if (lamports == (await connection.getBalance(account.publicKey))) {
+      console.log(`Airdrop finished after ${retries} retries`);
+      return account;
+    }
+    if (--retries <= 0) {
+      break;
+    }
+  }
+  throw new Error(`Airdrop of ${lamports} failed`);
+}
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
---
+++ programs/anchor-playground/src/lib.rs
@@ -0,0 +1,48 @@
+#![feature(proc_macro_hygiene)]
+
+use anchor_lang::prelude::*;
+
+// Define the program's instruction handlers.
+
+#[program]
+mod anchor_playground {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>, authority: Pubkey) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = authority;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+}
+
+// Define the validated accounts for each handler.
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init)]
+    pub counter: ProgramAccount<'info, Counter>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: ProgramAccount<'info, Counter>,
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+}
+
+// Define the program owned accounts.
+
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
