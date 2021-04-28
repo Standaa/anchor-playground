468cc2318ec1d2f7e1562f1f37390a009e94502a
Standaa <stanislasdrg@gmail.com>
2021-04-28 11:21:22 +0100

    feat: Add deployment script

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -1,12 +1,56 @@
-// Migrations are an early feature. Currently, they're nothing more than this
-// single deploy script that's invoked from the CLI, injecting a provider
-// configured from the workspace's Anchor.toml.
+import { Token } from "@solana/spl-token";
 
-const anchor = require("@project-serum/anchor");
+const { web3, setProvider, workspace, createMint } = require("@project-serum/anchor");
 
 module.exports = async function (provider) {
-  // Configure client to use the provider.
-  anchor.setProvider(provider);
+  setProvider(provider);
 
-  // Add your deploy script here.
+  const connection = provider.connection;
+  const deployer = provider.wallet.payer;
+  const accountAsSeed = new web3.Account();
+  const poolProgram = workspace.AnchorPlayground;
+
+  await poolProgram.state.rpc.new({
+    accounts: {},
+  });
+
+  // Create a PDA
+  const [poolAuthority, nonce] = await web3.PublicKey.findProgramAddress(
+    [accountAsSeed.publicKey.toBuffer()], // Random account used as seed
+    poolProgram.programId,
+  );
+
+  // connection: Connection - connection The connection to use
+  // payer: Account - payer Fee payer for transaction
+  // mintAuthority: PublicKey - mintAuthority Account or multisig that will control minting
+  // freezeAuthority: PublicKey - freezeAuthority Optional account or multisig that can freeze token accounts
+  // decimals: number - decimals Location of the decimal place
+  // programId: PublicKey - programId Optional token programId, uses the system programId by default
+
+  const poolMint: Token = await createMint(
+    connection,
+    deployer,
+    deployer,
+    deployer,
+    8,
+    poolProgram.programId,
+  );
+
+  await poolProgram.state.rpc.initialize(
+    nonce,
+    provider.wallet.publicKey,
+    provider.wallet.publicKey,
+    poolAuthority,
+    accountAsSeed.publicKey,
+    {
+      accounts: {
+        poolMint: poolMint.publicKey,
+      },
+    },
+  );
+
+  console.log("Nonce", nonce);
+  console.log("AccountAsSeed pubkey", accountAsSeed.publicKey.toBase58());
+  console.log("PoolMint pubkey", poolMint.publicKey.toBase58());
+  console.log("wallet publickey :", provider.wallet.publicKey.toBase58());
 };
