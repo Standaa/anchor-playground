9854e13df6a470f5f3b640dd4bd942b648abd481
Standaa <stanislasdrg@gmail.com>
2021-04-28 11:23:53 +0100

    test: Mint & Deposit flows

--- tests/anchor-playground.spec.ts
+++ tests/anchor-playground.spec.ts
@@ -1,45 +1,146 @@
 import assert from "assert";
-import * as anchor from "@project-serum/anchor";
+import { web3, workspace, setProvider, Provider, BN } from "@project-serum/anchor";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { PublicKey } from "@solana/web3.js";
+import { sleep } from "../app";
+// import { isInitialized, isSplAccountInitialized } from "../app";
 
 describe("anchor-playground", () => {
-  const provider = anchor.Provider.local();
+  const provider = Provider.local();
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(provider);
-
-  // Counter for the tests.
-  const counter = new anchor.web3.Account();
+  setProvider(provider);
 
   // Program for the tests.
-  const program = anchor.workspace.AnchorPlayground;
+  const program = workspace.AnchorPlayground;
+  // @ts-expect-error
+  const payer = provider.wallet.payer as web3.Account;
 
-  it("Creates a counter", async () => {
-    await program.rpc.create(provider.wallet.publicKey, {
-      accounts: {
-        counter: counter.publicKey,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      },
-      signers: [counter],
-      instructions: [await program.account.counter.createInstruction(counter)],
-    });
+  const ANCHOR_PLAYGROUND_SEED = Buffer.from("AnchorPlayground");
 
-    let counterAccount = await program.account.counter(counter.publicKey);
+  let poolAuthority: web3.PublicKey;
+  let nonce: number;
+  let poolTokenMint: Token;
+  let poolTokenAccount: web3.PublicKey;
 
-    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));
-    assert.ok(counterAccount.count.toNumber() === 0);
+  before(async () => {
+    const [_poolAuthority, _nonce] = await web3.PublicKey.findProgramAddress(
+      [ANCHOR_PLAYGROUND_SEED],
+      program.programId,
+    );
+
+    poolAuthority = _poolAuthority;
+    nonce = _nonce;
+
+    poolTokenMint = await Token.createMint(
+      provider.connection,
+      payer,
+      poolAuthority,
+      null,
+      8,
+      TOKEN_PROGRAM_ID,
+    );
+
+    poolTokenAccount = await poolTokenMint.createAccount(poolAuthority);
+    // await poolTokenMint.mintTo(poolTokenAccount, poolAuthority, [payer], 3000);
   });
 
-  it("Updates a counter", async () => {
-    await program.rpc.increment({
+  it("Initializes Pool correctly", async () => {
+    await program.state.rpc.new({
+      accounts: {},
+    });
+
+    const tx = await program.state.rpc.initialize(
+      nonce,
+      provider.wallet.publicKey,
+      provider.wallet.publicKey,
+      {
+        accounts: {
+          poolTokenMint: poolTokenMint.publicKey,
+          poolTokenAccount: poolTokenAccount,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          poolTokenMintAuthority: poolAuthority,
+        },
+      },
+    );
+
+    console.log(tx);
+
+    const poolState = await program.state();
+
+    assert.ok(poolState.nonce === nonce);
+    assert.ok(poolState.signer.equals(provider.wallet.publicKey));
+    assert.ok(poolState.authority.equals(provider.wallet.publicKey));
+    assert.ok(poolState.poolTokenMint.equals(poolTokenMint.publicKey));
+
+    const poolTokenAccountAfterMint = await poolTokenMint.getAccountInfo(poolTokenAccount);
+    console.log("poolTokenAccountAfterMint", poolTokenAccountAfterMint.amount.toNumber());
+
+    assert.ok(poolTokenAccountAfterMint.amount.toNumber() === 1000);
+  });
+
+  it("Deposits to a user account", async () => {
+    const poolState = await program.state();
+    const mintAddress: PublicKey = poolState.poolTokenMint;
+
+    // Refetch the token instead of getting poolTokenMint directly
+    const poolTokenMintAfterInit = new Token(
+      provider.connection,
+      mintAddress,
+      TOKEN_PROGRAM_ID,
+      payer,
+    );
+    // const poolTokenMintAfterInit = poolTokenMint;
+
+    // console.log("Supply", (await poolTokenMintAfterInit.getMintInfo()).supply.toNumber());
+
+    const userWallet = new web3.Account();
+
+    // const associatedUserAccount = await Token.getAssociatedTokenAddress(
+    //   ASSOCIATED_TOKEN_PROGRAM_ID,
+    //   TOKEN_PROGRAM_ID,
+    //   mintAddress,
+    //   userWallet.publicKey,
+    // );
+
+    console.log("userWallet", userWallet.publicKey.toBase58());
+    // console.log("associatedUserAccount", associatedUserAccount.toBase58());
+
+    // poolTokenMintAfterInit.createAssociatedTokenAccount
+
+    const userAssociatedAddress = await poolTokenMintAfterInit.createAssociatedTokenAccount(
+      userWallet.publicKey,
+    );
+
+    console.log("userAssociatedAddress", userAssociatedAddress.toBase58());
+
+    console.log("poolAuthority", poolAuthority.toBase58());
+    console.log("poolTokenAccount", poolTokenAccount.toBase58());
+
+    const tx = await program.state.rpc.deposit(new BN(12), {
       accounts: {
-        counter: counter.publicKey,
-        authority: provider.wallet.publicKey,
+        poolTokenMintAuthority: poolAuthority,
+        poolTokenAccount: poolTokenAccount,
+        userAssociatedTokenAccount: userAssociatedAddress,
+        tokenProgram: TOKEN_PROGRAM_ID,
       },
     });
 
-    const counterAccount = await program.account.counter(counter.publicKey);
+    console.log(tx);
 
-    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));
-    assert.ok(counterAccount.count.toNumber() == 1);
+    const poolTokenAccountAfterDeposit = await poolTokenMintAfterInit.getAccountInfo(
+      poolTokenAccount,
+    );
+
+    const userTokenAccountAfterDeposit = await poolTokenMintAfterInit.getAccountInfo(
+      userAssociatedAddress,
+    );
+
+    console.log("poolTokenAccountAfterDeposit", poolTokenAccountAfterDeposit.amount.toNumber());
+    console.log("userTokenAccountAfterDeposit", userTokenAccountAfterDeposit.amount.toNumber());
+
+    // assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));
+    // assert.ok(counterAccount.count.toNumber() === 0);
+    assert.ok(true);
   });
 });
